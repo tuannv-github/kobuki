31cc4bf971fb14a7365afe85c1905d10aba09f8d
Tuan V. Ngo <tuannv.email@gmail.com>
2025-03-26 23:17:58 +0800

    Add bring up package

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # kobuki
+
+```
+git clone https://github.com/tuannv-github/kobuki.git
+git submodule update --init --recursive
+colcon build --symlink-install
+```
+
+```
+sudo rosdep init
+rosdep update
+
+rosdep install --from-paths src --ignore-src -r -yjoyn
+```
+
+```
+git clone --recursive https://github.com/CollaborativeRoboticsLab/kobuki.git
+```
+
+```
+colcon build --merge-install --cmake-args -DBUILD_TESTING=OFF -DCMAKE_CXX_FLAGS="-Wno-error=vla"
+```
+
+```
+ros2 launch kobuki kobuki.launch.py
+```
+
+```
+ros2 run joy joy_node --ros-args -p dev:="/dev/input/js0" -r __node:=joy_ctrl -p deadzone:=0.05 -p autorepeat_rate:=20.0 --log-level info
+```
+
+```
+sudo apt update
+sudo apt install -y build-essential meson ninja-build git libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libx264-dev
+sudo apt install -y \
+  gstreamer1.0-tools \
+  gstreamer1.0-plugins-base \
+  gstreamer1.0-plugins-good \
+  gstreamer1.0-plugins-bad \
+  gstreamer1.0-plugins-ugly \
+  gstreamer1.0-libav \
+  libgstreamer1.0-dev \
+  libgstreamer-plugins-base1.0-dev \
+  libgstreamer-plugins-bad1.0-dev \
+  gstreamer1.0-x \
+  gstreamer1.0-alsa \
+  gstreamer1.0-gl \
+  gstreamer1.0-gtk3 \
+  gstreamer1.0-qt5 \
+  gstreamer1.0-pulseaudio
+gst-launch-1.0 v4l2src device=/dev/video0 ! videoconvert ! video/x-raw,format=I420 ! x264enc tune=zerolatency speed-preset=ultrafast bitrate=2000 ! flvmux streamable=true ! rtmpsink location="rtmp://10.1.101.210:/live/stream/kobuki" sync=false
+```
---
+++ kobuki_bringup/launch/kobuki.launch.py
@@ -0,0 +1,61 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    kobuki_node_launch    = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('kobuki_node'), 'launch', 'kobuki_node_mux.launch.py'))
+    )
+
+    joy_node =    Node(
+            package='joy',
+            executable='joy_node',
+            name='joy',  # Remapped node name
+            parameters=[
+                {'dev': '/dev/input/js0'},
+                {'deadzone': 0.05},
+                {'autorepeat_rate': 20.0},
+            ],
+            arguments=['--log-level', 'info'],
+        )
+
+    teleop_twist_joy_node = Node(
+            package='teleop_twist_joy',
+            executable='teleop_node',
+            name='teleop_twist_joy',
+            parameters=[
+                {'axis_linear.x': 1},  # Left stick vertical for linear.x
+                {'axis_angular.yaw': 0},  # Left stick horizontal for angular.z
+                {'scale_linear.x': 0.5},  # Max linear speed
+                {'scale_angular.yaw': 1.5},  # Max angular speed
+                {'enable_button': 0},  # Button to enable (e.g., L1)
+                {'enable_turbo_button': -1},  # Disable turbo
+            ],
+            remappings=[('/cmd_vel', '/joy_cmd_vel')],  # Remap /cmd_vel to /joy_cmd_vel
+        )
+
+    pkg_share = get_package_share_directory('kobuki_bringup')
+    config_file = os.path.join(pkg_share, 'config', 'twist_mux_topics.yaml')
+    twist_mux_node = Node(
+        package='twist_mux',
+        executable='twist_mux',
+        name='twist_mux',
+        output='screen',
+        parameters=[config_file],  # Pass the configuration file
+        remappings=[('/cmd_vel_out', '/mux/output/cmd_vel')]  # Remap output topic if needed
+    )
+
+    return LaunchDescription([
+            kobuki_node_launch,
+            joy_node,
+            teleop_twist_joy_node,
+            twist_mux_node,
+
+        ])
